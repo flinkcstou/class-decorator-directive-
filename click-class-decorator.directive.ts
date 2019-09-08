bf9c0b5d75209022eef19007e89050d66b6aa419
flinkcstou <42375834+flinkcstou@users.noreply.github.com>
2019-09-08 19:19:25 +0600

    Create click-class-decorator.directive.ts

---
+++ click-class-decorator.directive.ts
@@ -0,0 +1,40 @@
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
+
+@Directive({
+  selector: '[classDecorator]'
+})
+export class ClickClassDecoratorDirective {
+
+  @Input('appendingClasses')
+  appendingClasses: string[] | string;
+
+  constructor(private el: ElementRef) {
+  }
+
+  @HostListener('ionFocus', ['$event'])
+  ionFocusEvent() {
+    if (!this.appendingClasses) {
+      return;
+    }
+    if (typeof this.appendingClasses === 'string') {
+      this.el.nativeElement.classList.add(this.appendingClasses);
+    } else {
+      this.el.nativeElement.classList.add(...this.appendingClasses);
+    }
+  }
+
+  @HostListener('ionBlur', ['$event'])
+  ionBlurEvent(event) {
+    if (!this.appendingClasses) {
+      return;
+    }
+    if (typeof this.appendingClasses === 'string') {
+      this.el.nativeElement.classList.remove(this.appendingClasses);
+    } else {
+      for (const className of this.appendingClasses) {
+        this.el.nativeElement.classList.remove(className);
+      }
+    }
+  }
+
+}
